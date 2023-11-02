10eefdbb4f3fc33e7a2e3b51d1e670b123051e38
xxx <xxx@xxx.xxx>
2023-11-02 10:15:57 +0300

    1

--- order/views.py
+++ order/views.py
@@ -176,7 +176,7 @@ class GetOrdersByWorker(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         print(user.role.id)
-        return Order.objects.filter(users__in=[user.id], stage__role__in=[user.role.id])
+        return Order.objects.filter(is_done=False, users__in=[user.id], stage__role__in=[user.role.id])
 
 class GetOrdersByWorkerForCalendar(generics.ListAPIView):
     serializer_class = OrderSerializer
